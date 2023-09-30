51903bb6772fd609198802c54cc658a75514feb7
Yang <113082249+1234567Yang@users.noreply.github.com>
2023-09-29 21:09:21 -0700

    添加笔记框架

---
+++ lessons/ch/index.js
@@ -0,0 +1,40 @@
+function changeContent(TheLink){
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = Callback;
+    xmlHttp.open("GET", TheLink, false ); // read
+    xmlHttp.send(null); 
+}
+function Callback(){
+    if ( xmlHttp.readyState == 4 ) {
+        if ( xmlHttp.status == 200 ) {
+            var value = xmlHttp.responseText;
+            setButton(value);
+        }
+    }
+}
+function setButton(text){
+    text = text.split('\n');
+    console.log(text);
+    for(var sg of text){
+        var param = sg.split('|');
+        createButton(param[0], param[1], param[2]);
+    }
+}
+function createButton(url, title, time){
+    var btn = document.createElement("BUTTON");
+    btn.innerHTML = title;
+    btn.setAttribute("url", url);
+    btn.setAttribute("title", title);
+    btn.setAttribute("time", time);
+    btn.onclick = function(){
+        var url = "";
+        url = "/static/view/model.html";
+        url += "?" + 
+        "url=" + "/lessons/ch/" + btn.getAttribute("url") + "&" + 
+        "title=" + btn.getAttribute("title") + "&" + 
+        "time=" + btn.getAttribute("time");
+
+        window.open(url, "_blank");
+    }
+    document.body.appendChild(btn);
+}
